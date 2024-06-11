193ffcb17ad5731e088ba10b56ad0f24289d3b18
Charlie Rodrigues <152749609+liezim@users.noreply.github.com>
2024-06-11 12:02:29 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const contarVogais = (str) => {
+  str = str.toLowerCase();
+
+  const vogais = "aeiou";
+
+  let contagem = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (vogais.includes(str[i])){
+      contagem++;
+    }
+  }
+  return contagem;
+};
+
+const atualizarResultado = () => {
+  const inputString = document.getElementById('inputString').value;
+  const resultado = contarVogais(inputString);
+  document.getElementById('result').innerText = `0 Número de vogais digitado '${inputString}' é: ${resultado}`;
+
+} ;
+
+document.getElementById('countButton').addEventListener('click', atualizarResultado);
